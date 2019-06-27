19e3b87cdba46831d0e0d7944a2d65c7dc208738
MiglyA <zq6295migly@gmail.com>
2019-06-28 00:07:42 +0900

    [update]fix-typoブランチから移植

--- scripts/respeaker_node.py
+++ scripts/respeaker_node.py
@@ -3,60 +3,28 @@
 # Author: furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
 
 import angles
-from contextlib import contextmanager
 import usb.core
 import usb.util
+from pixel_ring import usb_pixel_ring_v2
 import pyaudio
 import math
 import numpy as np
 import tf.transformations as T
-import os
 import rospy
 import struct
-import sys
 import time
 from audio_common_msgs.msg import AudioData
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Bool, Int32, ColorRGBA
 from dynamic_reconfigure.server import Server
 try:
-    from pixel_ring import usb_pixel_ring_v2
-except IOError as e:
-    print e
-    raise RuntimeError("Check the device is connected and recognized")
-
-try:
     from respeaker_ros.cfg import RespeakerConfig
 except Exception as e:
     print e
-    raise RuntimeError("Need to run respeaker_gencfg.py first")
-
-
-# suppress error messages from ALSA
-# https://stackoverflow.com/questions/7088672/pyaudio-working-but-spits-out-error-messages-each-time
-# https://stackoverflow.com/questions/36956083/how-can-the-terminal-output-of-executables-run-by-python-functions-be-silenced-i
-@contextmanager
-def ignore_stderr(enable=True):
-    if enable:
-        devnull = None
-        try:
-            devnull = os.open(os.devnull, os.O_WRONLY)
-            stderr = os.dup(2)
-            sys.stderr.flush()
-            os.dup2(devnull, 2)
-            try:
-                yield
-            finally:
-                os.dup2(stderr, 2)
-                os.close(stderr)
-        finally:
-            if devnull is not None:
-                os.close(devnull)
-    else:
-        yield
-
+    print "Need to run respeaker_gencfg.py first"
 
 # Partly copied from https://github.com/respeaker/usb_4_mic_array
+
 # parameter list
 # name: (id, offset, type, max, min , r/w, info)
 PARAMETERS = {
@@ -214,10 +182,9 @@ class RespeakerInterface(object):
 
 
 class RespeakerAudio(object):
-    def __init__(self, on_audio, channel=0, suppress_error=True):
+    def __init__(self, on_audio, channel=0):
         self.on_audio = on_audio
-        with ignore_stderr(enable=suppress_error):
-            self.pyaudio = pyaudio.PyAudio()
+        self.pyaudio = pyaudio.PyAudio()
         self.channels = None
         self.channel = channel
         self.device_index = None
@@ -296,31 +263,27 @@ class RespeakerNode(object):
         rospy.on_shutdown(self.on_shutdown)
         self.update_rate = rospy.get_param("~update_rate", 10.0)
         self.sensor_frame_id = rospy.get_param("~sensor_frame_id", "respeaker_base")
-        self.doa_xy_offset = rospy.get_param("~doa_xy_offset", 0.0)
-        self.doa_yaw_offset = rospy.get_param("~doa_yaw_offset", 90.0)
+        self.doa_offset = rospy.get_param("~doa_offset", 90.0)
         self.speech_prefetch = rospy.get_param("~speech_prefetch", 0.5)
         self.speech_continuation = rospy.get_param("~speech_continuation", 0.5)
         self.speech_max_duration = rospy.get_param("~speech_max_duration", 7.0)
         self.speech_min_duration = rospy.get_param("~speech_min_duration", 0.1)
-        suppress_pyaudio_error = rospy.get_param("~suppress_pyaudio_error", True)
         #
         self.respeaker = RespeakerInterface()
         self.speech_audio_buffer = str()
         self.is_speeching = False
         self.speech_stopped = rospy.Time(0)
-        self.prev_is_voice = None
-        self.prev_doa = None
         # advertise
-        self.pub_vad = rospy.Publisher("is_speeching", Bool, queue_size=1, latch=True)
-        self.pub_doa_raw = rospy.Publisher("sound_direction", Int32, queue_size=1, latch=True)
-        self.pub_doa = rospy.Publisher("sound_localization", PoseStamped, queue_size=1, latch=True)
+        self.pub_vad = rospy.Publisher("is_speeching", Bool, queue_size=1)
+        self.pub_doa_raw = rospy.Publisher("sound_direction", Int32, queue_size=1)
+        self.pub_doa = rospy.Publisher("sound_localization", PoseStamped, queue_size=1)
         self.pub_audio = rospy.Publisher("audio", AudioData, queue_size=10)
         self.pub_speech_audio = rospy.Publisher("speech_audio", AudioData, queue_size=10)
         # init config
         self.config = None
         self.dyn_srv = Server(RespeakerConfig, self.on_config)
         # start
-        self.respeaker_audio = RespeakerAudio(self.on_audio, suppress_error=suppress_pyaudio_error)
+        self.respeaker_audio = RespeakerAudio(self.on_audio)
         self.speech_prefetch_bytes = int(
             self.speech_prefetch * self.respeaker_audio.rate * self.respeaker_audio.bitdepth / 8.0)
         self.speech_prefetch_buffer = str()
@@ -388,30 +351,24 @@ class RespeakerNode(object):
         is_voice = self.respeaker.is_voice()
         doa_rad = math.radians(self.respeaker.direction - 180.0)
         doa_rad = angles.shortest_angular_distance(
-            doa_rad, math.radians(self.doa_yaw_offset))
+            doa_rad, math.radians(self.doa_offset))
         doa = math.degrees(doa_rad)
 
         # vad
-        if is_voice != self.prev_is_voice:
-            self.pub_vad.publish(Bool(data=is_voice))
-            self.prev_is_voice = is_voice
+        self.pub_vad.publish(Bool(data=is_voice))
 
         # doa
-        if doa != self.prev_doa:
-            self.pub_doa_raw.publish(Int32(data=doa))
-            self.prev_doa = doa
+        self.pub_doa_raw.publish(Int32(data=doa))
 
-            msg = PoseStamped()
-            msg.header.frame_id = self.sensor_frame_id
-            msg.header.stamp = stamp
-            ori = T.quaternion_from_euler(math.radians(doa), 0, 0)
-            msg.pose.position.x = self.doa_xy_offset * np.cos(doa_rad)
-            msg.pose.position.y = self.doa_xy_offset * np.sin(doa_rad)
-            msg.pose.orientation.w = ori[0]
-            msg.pose.orientation.x = ori[1]
-            msg.pose.orientation.y = ori[2]
-            msg.pose.orientation.z = ori[3]
-            self.pub_doa.publish(msg)
+        msg = PoseStamped()
+        msg.header.frame_id = self.sensor_frame_id
+        msg.header.stamp = stamp
+        ori = T.quaternion_from_euler(math.radians(doa), 0, 0)
+        msg.pose.orientation.w = ori[0]
+        msg.pose.orientation.x = ori[1]
+        msg.pose.orientation.y = ori[2]
+        msg.pose.orientation.z = ori[3]
+        self.pub_doa.publish(msg)
 
         # speech audio
         if is_voice:
